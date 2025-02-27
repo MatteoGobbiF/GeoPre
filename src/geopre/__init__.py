7239255745e473c1913e9406bfc1fbfee296e488
Matteo Gobbi Frattini <matteo.gf@live.it>
2025-02-27 20:51:39 +0100

    Create __init__.py

---
+++ src/geopre/__init__.py
@@ -0,0 +1,22 @@
+from .cloud_masking import mask_clouds_S2, mask_clouds_landsat
+from .scaling_reproject_mask import (
+    Z_score_scaling,
+    Min_Max_Scaling,
+    get_crs,
+    compare_crs,
+    reproject_data,
+    mask_raster_data,
+)
+from .stacking import stack_bands
+
+__all__ = [
+    "mask_clouds_S2",
+    "mask_clouds_landsat",
+    "Z_score_scaling",
+    "Min_Max_Scaling",
+    "get_crs",
+    "compare_crs",
+    "reproject_data",
+    "mask_raster_data",
+    "stack_bands",
+]
